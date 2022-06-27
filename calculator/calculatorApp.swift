aaf2afef28899d3c259a0a139259bd97a4106045
Alona <albsch57@gmail.com>
2022-06-27 18:34:39 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Alona on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
